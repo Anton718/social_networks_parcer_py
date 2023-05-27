debdaca227d80df2d8c90d9e4732e4a636a6da31
Anton718 <anton.kostroma@yahoo.com>
2023-05-26 22:40:48 -0400

    add uppdates

--- dbCleaner.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def dbCleaner():
-    conn = sqlite3.connect('urls.db')
-    cur = conn.cursor()
-    deletion = cur.execute('delete from urls')
-    conn.commit()
-    conn.close()
--- pythonParser.py
+++ pythonParser.py
@@ -1,15 +1,18 @@
 import urllib.request
 import sqlite3
 import re
-import dbCleaner
-import searchMedia
 
-dbCleaner.dbCleaner()
+
+conn = sqlite3.connect('urls.db')
+cur = conn.cursor()
+cur.execute('delete from urls')
+conn.commit()
+conn.close()
 
 try:
     print('start')
 
-    web = 'https://www.avtosushi.ru'
+    web = 'https://www.cnn.com'
 
     def my_function(item):
         get_url = urllib.request.urlopen(item)
@@ -26,10 +29,44 @@ try:
 
     my_function(web)
     
-    searchMedia.searchLinks()
+   
+    conn = sqlite3.connect('urls.db')
+    curs = conn.cursor()
+    links = curs.execute('select url from urls').fetchall()
+    my_tuple = ()
+    for link in links:
+        my_tuple += link
+    for i in my_tuple:
+        if len(i) < 80:
+            if 'insta' in i:
+                print(i)
+            elif 'facebook' in i:
+                print(i)
+            elif 'youtube' in i:
+                print(i)
+            elif 'linkedin' in i:
+                print(i)
+            elif 'twitter' in i:
+                print(i)
+            elif 'vkontakte' in i:
+                print(i)
+            elif 'vk.ru' in i:
+                print(i)
+            elif 'vk.com' in i:
+                print(i)
+            elif 'ok.ru' in i:
+                print(i)
+            elif 'odnoklass' in i:
+                print(i)
+            elif 'telegram' in i:
+                print(i)
+            elif 'whatsapp' in i:
+                print(i)
+            elif 'reddit' in i:
+                print(i)
 
 except:
-    print('some problem')
+    print('error')
 
 finally:
     print('end')
